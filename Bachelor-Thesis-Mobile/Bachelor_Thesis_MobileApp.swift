9bb4e73a617e86006ad077f6229ece1a9107d0bc
Lirha <brinzila.lisa@gmail.com>
2022-05-06 22:34:28 +0300

    Initial Commit

---
+++ Bachelor-Thesis-Mobile/Bachelor_Thesis_MobileApp.swift
@@ -0,0 +1,20 @@
+//
+//  Bachelor_Thesis_MobileApp.swift
+//  Bachelor-Thesis-Mobile
+//
+//  Created by Lisa Brinzila on 06.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Bachelor_Thesis_MobileApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
